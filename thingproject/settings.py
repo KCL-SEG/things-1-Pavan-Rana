1fe4b65302ada4f31ce52a4518caf7c56825c26e
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-12-01 17:24:00 +0000

    done

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -123,6 +123,6 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-AUTH_THINGS_MODEL = 'things.Things'
+AUTH_USER_MODEL = 'things.Things'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
