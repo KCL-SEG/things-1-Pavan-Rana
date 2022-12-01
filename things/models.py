fce3222fb86011d01809587c4f613e1adc1245cf
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-12-01 17:56:48 +0000

    DONE

--- things/models.py
+++ things/models.py
@@ -1,7 +1,15 @@
-from django.db import models.Model
-from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(Model):
-    name = models.TextField(unique=True,blank=False,max_length=30)
-    description = models.TextField(max_length=120)
-    quantity = models.IntegerField()
+class Thing(models.Model):
+    REQUIRED_FIELDS = ('quantity',)
+    USERNAME_FIELD = 'name'
+    is_anonymous = False
+    is_authenticated = True
+
+    name = models.CharField(unique=True,blank=False,max_length=30)
+    description = models.CharField(blank=True, max_length=120)
+    quantity = models.IntegerField(validators=[
+            MaxValueValidator(100),
+            MinValueValidator(0)
+        ])
