2449a68ff82396abea93cf97930d0c8ac9e7b431
Pavan-Rana <pavan.rana@kcl.ac.uk>
2022-12-01 17:35:15 +0000

    done

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
-from django.db import models
+from django.db import models.Model
 from django.contrib.auth.models import AbstractUser
 
-class Thing(AbstractUser):
+class Thing(Model):
     name = models.TextField(unique=True,blank=False,max_length=30)
     description = models.TextField(max_length=120)
     quantity = models.IntegerField()
